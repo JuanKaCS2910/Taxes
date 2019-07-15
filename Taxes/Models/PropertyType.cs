f6cb2beb0f52aaf0330278903c10796760eeeea5
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2019-07-15 10:45:44 -0500

    Create Proyect

---
+++ Taxes/Models/PropertyType.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Taxes.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class PropertyType
+    {
+        [Key]
+        public int PropertyId { get; set; }
+
+        [Required(ErrorMessage ="The field {0} is required")]
+        public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+    }
+}
---
+++ Taxes/Models/TaxesContent.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace Taxes.Models
+{
+    public class TaxesContent : DbContext
+    {
+        public TaxesContent() : base("DefaultConnection")
+        {
+        }
+
+        /*
+        El método dispose es importante para cerrar la conexxión a la BD. 
+        */
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);    
+        }
+
+        public System.Data.Entity.DbSet<Taxes.Models.PropertyType> PropertyTypes { get; set; }
+    }
+}
